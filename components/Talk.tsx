7e6f070e88f6b876395ae2abc0fb6c553717f7b1
dudduss <sampathh.s@gmail.com>
2022-12-26 16:12:29 -0800

    some styling

--- components/Talk.tsx
+++ components/Talk.tsx
@@ -1,18 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { Button, Textarea } from "@chakra-ui/react";
+import { Button, Textarea, Box, Text, calc } from "@chakra-ui/react";
 import createSpeechServicesPonyfill from "web-speech-cognitive-services";
 import createSpeechSynthesisPonyfill from "web-speech-cognitive-services";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import * as speechsdk from "microsoft-cognitiveservices-speech-sdk";
+import { Message } from "../public/schemas";
+
+export function MessageBox({ message }: { message: Message }) {
+  const backgroundColor = message.isFromGpt ? "gray.200" : "blue.200";
+  const sender = message.isFromGpt ? "Linda" : "Me";
+
+  return (
+    <Box marginTop={10} marginBottom={10}>
+      <Text fontSize="2xl"> {sender} </Text>
+      <Box bg={backgroundColor} padding={10} borderRadius={20} marginTop={2}>
+        <Text fontSize="2xl">{message.text}</Text>
+      </Box>
+    </Box>
+  );
+}
 
 export default function Talk() {
   const [gptResponse, setGptResponse] = useState("");
   const [isListening, setIsListening] = useState(false);
-  const [conversation, setConversation] = useState<string[]>([
-    "hello my name is",
-    "i want to practice spanish",
+  const [conversation, setConversation] = useState<Message[]>([
+    {
+      id: "some-id",
+      text: "hello my name is hello my name is hello my name is hello my name is hello my name ishello my name ishello my name ishello my name ishello my name is",
+      isFromGpt: false,
+    },
+    {
+      id: "some-id2",
+      text: "thanks for coming thanks for comingthanks for comingthanks for comingthanks for comingthanks for coming",
+      isFromGpt: true,
+    },
+    {
+      id: "some-id",
+      text: "hello my name is hello my name is hello my name is hello my name is hello my name ishello my name ishello my name ishello my name ishello my name is",
+      isFromGpt: false,
+    },
+    {
+      id: "some-id2",
+      text: "thanks for coming thanks for comingthanks for comingthanks for comingthanks for comingthanks for coming",
+      isFromGpt: true,
+    },
+    {
+      id: "some-id",
+      text: "hello my name is hello my name is hello my name is hello my name is hello my name ishello my name ishello my name ishello my name ishello my name is",
+      isFromGpt: false,
+    },
+    {
+      id: "some-id2",
+      text: "thanks for coming thanks for comingthanks for comingthanks for comingthanks for comingthanks for coming",
+      isFromGpt: true,
+    },
   ]);
 
   useEffect(() => {
@@ -80,34 +123,41 @@ export default function Talk() {
   }
 
   return (
-    <div>
-      <Button
-        onClick={() => {
-          setIsListening(true);
-          startListening();
-        }}
-        disabled={isListening}
-      >
-        Start
-      </Button>
-      <Button
-        onClick={() => {
-          setIsListening(false);
-          stopListening();
-          sendToGpt(transcript);
-          resetTranscript();
-        }}
-        disabled={!isListening}
-      >
-        Stop
-      </Button>
-      <Textarea
-        value={transcript}
-        size="lg"
-        resize={"vertical"}
-        readOnly
-      ></Textarea>
-      <p>{gptResponse}</p>
-    </div>
+    <Box>
+      <Box overflow={"scroll"} maxHeight={700} width={"70%"} margin={"auto"}>
+        {conversation.map((message, index) => (
+          <MessageBox key={index} message={message} />
+        ))}
+      </Box>
+      <Box marginTop={20} position={"relative"}>
+        <Button
+          onClick={() => {
+            setIsListening(true);
+            startListening();
+          }}
+          disabled={isListening}
+        >
+          Start
+        </Button>
+        <Button
+          onClick={() => {
+            setIsListening(false);
+            stopListening();
+            // sendToGpt(transcript);
+            resetTranscript();
+          }}
+          disabled={!isListening}
+        >
+          Stop
+        </Button>
+        <Textarea
+          value={transcript}
+          size="lg"
+          resize={"vertical"}
+          readOnly
+        ></Textarea>
+        <p>{gptResponse}</p>
+      </Box>
+    </Box>
   );
 }
